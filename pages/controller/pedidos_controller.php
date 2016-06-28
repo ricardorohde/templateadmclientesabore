04ccb622194c376090610d6ae6830b932917779c
Allan <wilanetoneti@gmail.com>
2016-06-28 18:59:40 -0300

    ALLAN - Explicações Wilson

---
+++ pages/controller/pedidos_controller.php
@@ -0,0 +1,34 @@
+<?php
+    require_once('function\function.php');
+    $error = false;
+    $success= false;
+    $mensagem = '';
+    $cliente_id = '4';
+    $data_registro = date ("Y-m-d H:i:s");
+    $listaPedidos = array();
+    $pedidos = array();
+    $arrayDados = array();    
+    $pagina = RetornaURL();
+
+
+    if($pagina == 'ped_andamento'){                             
+
+        $arrayDados = array('cliente_id'=>$cliente_id);
+        $listaPedidos = GoCURL($arrayDados, 'pedidos/em-andamento');
+
+    }
+
+    if($pagina == 'ped_detalhes_andamento'){
+
+    	if(!empty($_POST['pedidoID']))
+        {        
+            $arrayDados = array('cliente_id'=>$cliente_id, 'id'=>$_POST['pedidoID']);
+            $pedidos = GoCURL($arrayDados, 'pedidos/visualizar'); 
+            
+            //echo '<Pre>';print_r($pedidos);exit;
+        }    
+
+    }    
+
+?>
+ 
